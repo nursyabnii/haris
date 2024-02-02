3b2c463b9ab1524fff3e8f88f106bd7d94120c0f
nursyabnii <baniharis12@gmail.com>
2024-02-02 10:18:07 +0700

    initail commit

---
+++ web/components/home/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <!-- Hero Section Start -->
+    <section id="home" class="pt-36">
+        <div class="container">
+            <div class="flex flex-wrap">
+                <div class="w-full self-center px-4 lg:w-1/2">
+                    <h1 class="text-base font-semibold text-primary md:text-xl lg:text-2xl">Hello Everyone 👋, Saya <span class="block font-bold text-dark text-4xl">Muhammad Nur Sya'bani Haris</span> </h1>
+                    <h2 class="font-medium text-slate-500 text-lg mb-5">Web Developer & <span class="text-dark">Unity Programmer</span></h2>
+                    <p class="font-medium text-slate-400 mb-10 leading-relaxed">Learning web programming & unity programming is easy and fun, <span class="text-dark font-bold">isn't it!</span></p>
+                    <a href="#" class="text-base font-semibold bg-primary text-white py-3 px-8 rounded-full hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out">Contact Me</a>
+                </div>
+                <div class="w-full self-end px-4 lg:w-1/2">
+                    <div class="relative mt-10">
+                        <img src="assets/img/haris.png" alt="haris" class="max-w-full mx-auto"/>
+                        <span class="absolute bottom-20 -z-10 left-1/2 -translate-x-1/2">
+                            <svg width="400" height="400" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+                                <path fill="#3b82f6" d="M47.2,-52.1C61.4,-44.2,73.5,-29.7,77.6,-12.7C81.8,4.2,78,23.6,68.5,38.9C58.9,54.3,43.5,65.8,27.7,68C11.9,70.2,-4.3,63.2,-16.2,54.5C-28.2,45.9,-36,35.6,-41.6,24.6C-47.2,13.7,-50.5,2.1,-50.2,-10.5C-50,-23.1,-46.1,-36.7,-37.1,-45.5C-28.2,-54.3,-14.1,-58.3,1.2,-59.7C16.5,-61.1,32.9,-60,47.2,-52.1Z" transform="translate(100 100) scale(1.1)" />
+                            </svg>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Hero Section End -->
+</template>
---
+++ web/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3b82f6',
+        dark: '#0f172a',
+      },
+    },
+  },
+  plugins: [],
+}
+
